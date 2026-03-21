75d78ce649269640e92ba1807e79222a3dc4f5fd
ElMunto <elmunto@hotmail.co.uk>
2026-03-21 10:30:05 +0000

    Configurable AmmoPickup: amount and dialogue
    Add configurable ammo amount and an optional reload-dialogue toggle to AmmoPickup. AmmoPickup.cs: add startReloadDialogue (bool) and ammoAmount (int) with tooltips; use ammoAmount for GunHandler.AddAmmo instead of maxAmmoSize, and only call dialogueRunner.StartDialogue("Reloading") when startReloadDialogue is true and dialogueRunner is not null. Update scene prefab instance to set startReloadDialogue to true. These changes allow pickups to vary ammo granted and avoid null dialogueRunner calls.

--- Assets/Scripts/AmmoPickup.cs
+++ Assets/Scripts/AmmoPickup.cs
@@ -6,13 +6,22 @@ using Yarn.Unity;
 public class AmmoPickup : MonoBehaviour
 {
     public DialogueRunner dialogueRunner;
+    [Tooltip("Should picking up this ammo start the reload dialogue?")]
+    public bool startReloadDialogue = true;
+
+    [Tooltip("How much ammo to add on pickup?")]
+    public int ammoAmount = 10;
+
     private void OnTriggerEnter(Collider other)
     {
         GunHandler gunHandler = other.gameObject.GetComponent<GunHandler>();
         if (gunHandler)
         {
-            gunHandler.AddAmmo(gunHandler.maxAmmoSize);
-            dialogueRunner.StartDialogue("Reloading");
+            gunHandler.AddAmmo(ammoAmount);
+            if (startReloadDialogue && dialogueRunner != null)
+            {
+                dialogueRunner.StartDialogue("Reloading");
+            }
             Destroy(gameObject);
         }
     }
